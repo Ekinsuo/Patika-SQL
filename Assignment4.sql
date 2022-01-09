5d9df4017fbf1681da1f58c521755bb899e8bd39
Ekinsuo <97318595+Ekinsuo@users.noreply.github.com>
2022-01-09 21:42:39 +0300

    Update Assignment4.sql

--- Assignment4.sql
+++ Assignment4.sql
@@ -4,8 +4,8 @@ select distinct replacement_cost from film;
 select count (replacement_cost) from film;
 
 --film tablosunda bulunan film isimlerinde (title) kaç tanesini T karakteri ile başlar ve aynı zamanda rating 'G' ye eşittir?
---select count(title) from film
---where title like 'T%'and rating = 'G';
+select count(title) from film
+where title like 'T%'and rating = 'G';
 --country tablosunda bulunan ülke isimlerinden (country) kaç tanesi 5 karakterden oluşmaktadır?
 select count (*) from country
 where country like '_____';
