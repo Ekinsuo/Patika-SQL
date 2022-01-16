031f2d0d8f6a96de98f326979cf4393835779992
Ekinsu Özmen <97318595+Ekinsuo@users.noreply.github.com>
2022-01-16 23:42:03 +0300

    Assignment 6

--- Aggregate Functions.sql
+++ Aggregate Functions.sql
@@ -9,4 +9,3 @@ where rental_rate = 0.99;
 --film tablosunda bulunan filmlerin uzunluğu 150 dakikadan büyük olanlarına ait kaç farklı replacement_cost değeri vardır?
 select count (distinct replacement_cost) from film
 where length > 150;
-
